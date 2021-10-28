8651a4ea7d175d42b41311ca2cfa14e0d079aff3
jkmartin5500 <jkmartin5500@gmail.com>
2021-10-28 18:36:50 -0400

    Reformatted code/imports a bit, made command asynchronous

--- Chess_commands.py
+++ Chess_commands.py
@@ -1,23 +1,22 @@
 import datetime
 
-import discord
 from discord.ext import commands
 
 import chessdotcom
-
-gamemodes = {"blitz": "chess_blitz", "rapid": "chess_rapid", "bullet": "chess_bullet", "daily": "chess_daily"}
-
 
 class Chess(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        chessdotcom.Client.aio = True
+        self.gamemodes = {"blitz": "chess_blitz", "rapid": "chess_rapid", "bullet": "chess_bullet", "daily": "chess_daily"}
 
     @commands.command(name="chessinfo", description="Gets player info on chess.com from given username")
     async def _info(self, ctx, *args):
         try:
             player, mode = args
-            stats = chessdotcom.client.get_player_stats(player).json[gamemodes[mode]]
+            response = await chessdotcom.client.get_player_stats(player)
+            stats = response.json['stats'][self.gamemodes[mode]]
             message = f"Player: {player} | Mode: {mode}\n"
             current = stats['last']['rating']
             current_date = datetime.date.fromtimestamp(stats['last']['date'])
